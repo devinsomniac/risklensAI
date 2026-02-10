df65d8ef2911330e08e5723b0e00c50d7d65edb8
devinsomniac <inzamamchowdhury20@gmail.com>
2026-02-10 21:57:55 +0000

    feat: Added vercel analytics

--- frontend/risklens_ui/app/layout.tsx
+++ frontend/risklens_ui/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
-
+import { Analytics } from "@vercel/analytics/next"
 
 
 export const metadata: Metadata = {
